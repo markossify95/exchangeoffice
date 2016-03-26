da303cc21740328c2acc865818529e3d00fe744e
markossify95 <markoss95@hotmail.com>
2016-03-26 21:59:22 +0100

    Redefined equals, hashCode & toString @ ExchangeOffice class

--- ExchangeOffice/src/exchangeoffice/ExchangeOffice.java
+++ ExchangeOffice/src/exchangeoffice/ExchangeOffice.java
@@ -4,6 +4,7 @@ import java.util.LinkedList;
 import exchangeoffice.currency.Currency;
 
 public class ExchangeOffice {
+	private String Name;
 	private LinkedList<Currency> list;
 	public ExchangeOffice() {
 	}
@@ -12,12 +13,47 @@ public class ExchangeOffice {
 		this.list = list;
 	}
 
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
 	public LinkedList<Currency> getList() {
 		return list;
 	}
 
 	public void setList(LinkedList<Currency> list) {
 		this.list = list;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Name == null) ? 0 : Name.hashCode());
+		result = prime * result + ((list == null) ? 0 : list.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if(obj instanceof ExchangeOffice){
+			ExchangeOffice other = (ExchangeOffice) obj;
+			if(other.getName().equals(this.getName())){
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "ExchangeOffice [Name of the office:" + Name;
 	}
 
 }
