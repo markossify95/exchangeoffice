8c4c82b54c00e60018ee44420359a9d888509be2
markossify95 <markoss95@hotmail.com>
2016-03-28 01:32:47 +0200

    Finished the implementation of all methods of the Test class.

--- ExchangeOffice/src/test/Test.java
+++ ExchangeOffice/src/test/Test.java
@@ -10,18 +10,36 @@ public class Test implements EOInterface {
 
 	@Override
 	public void addEROnDay(Currency cur, ExchangeRates er) {
+		if(cur == null || er == null){
+			throw new RuntimeException("Error! Not a valid entry.");
+		}
+		cur.getListOfRates().add(er);
 	}
 
 	@Override
 	public void deleteEROnDay(Currency cur, Date date) {
-		// TODO Auto-generated method stub
+		if(cur == null || date == null || date.after(new Date())){
+			throw new RuntimeException("Error! Not a valid entry.");
+		}
 
+		for (int i = 0; i < cur.getListOfRates().size(); i++) {
+			if(cur.getListOfRates().get(i).getDate().equals(date)){
+				cur.getListOfRates().remove(i);
+				return;
+			}
+		}
 	}
 
 	@Override
 	public ExchangeRates findAndReturn(Currency cur, Date date) {
-		// TODO Auto-generated method stub
+		if(cur == null || date == null || date.after(new Date())){
+			throw new RuntimeException("Error! Not a valid entry.");
+		}
+		for (int i = 0; i < cur.getListOfRates().size(); i++) {
+			if(cur.getListOfRates().get(i).getDate().equals(date)){
+				return cur.getListOfRates().get(i);
+			}
+		}
 		return null;
 	}
-
 }
